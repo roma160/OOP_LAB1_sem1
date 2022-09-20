d59bd636a726784942e5bd003accd651b86cbaf0
super mario <mario160@mail.ru>
2022-09-21 01:58:00 +0300

    Method3+4 implemented

--- LAB1.cpp
+++ LAB1.cpp
@@ -4,7 +4,7 @@
 #include <map>
 #include <stdio.h>
 
-typedef unsigned long long tn;
+typedef long long tn;
 
 using namespace std;
 
@@ -47,6 +47,8 @@ void printHistogram(const vector<double>& numbers, double start, double stopExcl
 	}
 }
 
+//vector<vector<double>(*)()>
+
 // METHOD1
 // 31)
 // const tn n, tn X0 = 2147483,
@@ -55,6 +57,8 @@ void printHistogram(const vector<double>& numbers, double start, double stopExcl
 // 32)
 // const tn n, tn X0 = 42949672,
 // const tn m = 4294967296, const tn c = 4294967291, const tn a = 4294967157
+//
+// const tn m = 4294967291, const tn c = 4294967279, const tn a = 4294967231
 
 vector<double> method1(
 	const tn n, tn X0 = 42949672,
@@ -87,8 +91,57 @@ vector<double> method2(
 	return ret;
 }
 
+vector<double> method3(
+	const tn n, tn X0 = 1247437, tn X1 = 224743647, const tn m = 4294967291)
+{
+	vector<double> ret(n);
+	tn buff;
+	for (tn i = 0; i < n; i++)
+	{
+		buff = (X1 + X0) % m;
+		X0 = X1; X1 = buff;
+		ret[i] = (double)buff / m;
+	}
+	return ret;
+}
+
+tn inverse(tn x, tn m)
+{
+	tn m0 = m;
+	tn a = 0, b = 1, buff;
+	while (x > 1 && m > 0) {
+		buff = a;
+		a = b - x / m * a;
+		b = buff;
+
+		buff = m;
+		m = x % m;
+		x = buff;
+	}
+	if (b < 0)
+		b += m0;
+	return b;
+}
+// METHOD4
+// 32)
+// const tn n, tn X0 = 42949672,
+// const tn m = 4294967296, const tn c = 4294967290, const tn a = 4294967157
+vector<double> method4(
+	const tn n, tn X0 = 42949672,
+	const tn m = 4294967197, const tn c = 4294967291, const tn a = 4294967157)
+{
+	vector<double> ret(n);
+	tn buff;
+	for (tn i = 0; i < n; i++)
+	{
+		X0 = (a * inverse(X0, m) % m + c) % m;
+		ret[i] = (double)X0 / m;
+	}
+	return ret;
+}
+
 int main()
 {
-	printList(method2(100));
-	printHistogram(method2(10000), 0, 1);
+	printList(method4(100));
+	printHistogram(method4(10000), 0, 1);
 }
